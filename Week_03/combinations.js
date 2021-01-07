1b4da1e6b9da05bfc716f4926095052a3f228a50
huangyong <1039113913@qq.com>
2021-01-07 09:14:38 +0800

    重新学习回溯

--- Week_03/combinations.js
+++ Week_03/combinations.js
@@ -3,6 +3,12 @@
  * @param {number} k
  * @return {number[][]}
  */
+
+
+
+//  方法1， 递归
+
+
 var combine = function(n, k) {
     const result = [];
     helper(n, k, [], result);
@@ -20,9 +26,36 @@ var helper = function(n, k, arr, res){
     }
     // process
 
-    //drill down 
-    helper(n-1, k, [...arr], res);
+    //drill down    对当前数字选， 或者不选
+    helper(n-1, k, [...arr], res); 
     arr.push(n);
     helper(n-1, k, [...arr], res);
     //revesee
 }
+
+
+//  方法2  回溯
+
+
+var combine = function(n, k) {
+    const res = [];
+    if(n<=0 || k <= 0) return false;
+    backTrack(n, k, [], res);
+    return res;
+};
+
+var backTrack = function(n, k, arr, res){
+    if(arr.length === k){
+        res.push([...arr]);
+    }
+
+    while(n){
+        arr.push(n);
+        backTrack(n-1, k, arr, res);
+        arr.pop();
+        n--;
+    }
+
+}
+
+
--- Week_03/permutations.js
+++ Week_03/permutations.js
@@ -27,3 +27,35 @@ const arr = [1, 2, 3];
 const res = permute(arr);
 
 console.log(res);
+
+
+// 方法2 回溯
+
+
+// 回溯
+var permute = function(nums) {
+    const res = [];
+    if(nums.length === 0){
+        return res;
+    }
+    const used = new Array(nums.length).fill(false);
+    dfs(nums, [], used, res);
+    return res;
+};
+
+var dfs = function(nums, arr, used, res) {
+    if(arr.length === nums.length ) {
+        res.push([...arr]);
+        return 
+    }
+    for(let i = 0; i < nums.length; i++) {
+        if(used[i]) {
+            continue;
+        }
+        arr.push(nums[i]);
+        used[i] = true;
+        dfs(nums, arr, used, res);
+        arr.pop();
+        used[i] = false;
+    }
+}
